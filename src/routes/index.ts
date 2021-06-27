42d674a9f00c3aa52c6259ebd9e4630026b754c2
Ketav Chotaliya <ketav.chotaliya@smartsensesolutions.com>
2021-06-27 13:47:43 +0530

    Add Rate Limiter for Public API

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,7 +4,17 @@ import { microservice, identity } from '../services';
 import gatewayConfig from '../config';
 import { createResponse } from '../utils/helper';
 import HttpStatus from 'http-status-codes';
+import rateLimit from 'express-rate-limit';
 export const router = express.Router();
+
+/**
+ * Create Rules of Rate Limit for Public API
+ */
+const publicAPI_RateLimit = rateLimit({
+  windowMs: gatewayConfig.server.rateLimit.retryAfter * 60 * 1000, // 15 min window
+  max: gatewayConfig.server.rateLimit.maximumTry, // start blocking after 10 requests
+  message: gatewayConfig.server.rateLimit.message,
+});
 
 router.get('/health', (req: CustomRequest, res: CustomResponse) => {
   res.status(200).json({ success: true });
@@ -19,7 +29,7 @@ for (let privatePath of gatewayConfig.gateway.url.privatePath) {
 
 // Redirect public route directally without any authentication
 for (let publicPath of gatewayConfig.gateway.url.publicPath) {
-  router.all(publicPath, (req: CustomRequest, res: CustomResponse) => {
+  router.all(publicPath, publicAPI_RateLimit, (req: CustomRequest, res: CustomResponse) => {
     microservice.callService(req, res);
   });
 }
