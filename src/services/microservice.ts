a89be2f36954faef7a661e06c52a798404c49a7c
Ketav Chotaliya <ketav.chotaliya.ext@siemens.com>
2022-12-18 10:40:34 +0530

    Exclude request body if method type is GET or HEAD

--- src/services/microservice.ts
+++ src/services/microservice.ts
@@ -1,8 +1,8 @@
-import { Response } from 'express';
-import got from 'got';
-import logger from '../utils/logger';
-import FormData from 'form-data';
-import HttpStatus, {getStatusText} from 'http-status-codes';
+import { Response } from "express";
+import got from "got";
+import logger from "../utils/logger";
+import FormData from "form-data";
+import HttpStatus, { getStatusText } from "http-status-codes";
 
 class Microservice {
   public async callService(req: any, res: Response) {
@@ -18,60 +18,72 @@ class Microservice {
       };
 
       let gotObject: any = {};
-
-      // send urlencoded or application/json data
-      if (['application/json', 'application/x-www-form-urlencoded'].includes(req.headers['content-type'])) {
-        gotObject = {
-          method: req.method,
-          json: true,
-          body: req.body,
-          headers: reqHeader,
-        };
-      }
-      // Send form-data
-      else {
-        // Create form-data object
-        const formData = new FormData();
-
-        // Append request body into form-data
-        for (let key in req.body) {
-          formData.append(key, req.body[key]);
+      // Do not add Request Body if Method type is GET or HEAD
+      if (!["GET", "HEAD"].includes(req.method)) {
+        // send urlencoded or application/json data
+        if (
+          ["application/json", "application/x-www-form-urlencoded"].includes(
+            req.headers["content-type"]
+          )
+        ) {
+          gotObject = {
+            method: req.method,
+            json: true,
+            body: req.body,
+            headers: reqHeader,
+          };
         }
+        // Send form-data
+        else {
+          // Create form-data object
+          const formData = new FormData();
 
-        // append request files into form-data
-        for (let key in req.files) {
-          // append single file
-          if (!Array.isArray(req.files[key])) {
-            formData.append(key, req.files[key].data, req.files[key].name);
+          // Append request body into form-data
+          for (let key in req.body) {
+            formData.append(key, req.body[key]);
           }
-          // append array of files (multiple files)
-          else {
-            for (let i = 0; i < req.files[key].length; i++) {
-              formData.append(`${key}[]`, req.files[key][i].data, req.files[key][i].name);
+
+          // append request files into form-data
+          for (let key in req.files) {
+            // append single file
+            if (!Array.isArray(req.files[key])) {
+              formData.append(key, req.files[key].data, req.files[key].name);
+            }
+            // append array of files (multiple files)
+            else {
+              for (let i = 0; i < req.files[key].length; i++) {
+                formData.append(
+                  `${key}[]`,
+                  req.files[key][i].data,
+                  req.files[key][i].name
+                );
+              }
             }
           }
-        }
 
-        gotObject = {
-          method: req.method,
-          body: formData,
-          headers: reqHeader,
-        };
+          gotObject = {
+            method: req.method,
+            body: formData,
+            headers: reqHeader,
+          };
+        }
       }
 
       const apiResponse: any = await got(microserviceUrl, gotObject);
 
-      res.setHeader('Content-Type', apiResponse.headers['content-type']);
+      res.setHeader("Content-Type", apiResponse.headers["content-type"]);
       return res.status(apiResponse.statusCode).send(apiResponse.body);
     } catch (e) {
-      logger.error(__filename, '', 'callService', 'Internal Server error', e);
-      if(e && e.headers && e.headers['content-types']) {
-        res.setHeader('Content-Type', e.headers['content-type']);
+      logger.error(__filename, "", "callService", "Internal Server error", e);
+      if (e && e.headers && e.headers["content-types"]) {
+        res.setHeader("Content-Type", e.headers["content-type"]);
       }
-      if(e && e.body) {
+      if (e && e.body) {
         res.status(e.statusCode).send(e.body);
       } else {
-        res.status(HttpStatus.INTERNAL_SERVER_ERROR).send(getStatusText(HttpStatus.INTERNAL_SERVER_ERROR));
+        res
+          .status(HttpStatus.INTERNAL_SERVER_ERROR)
+          .send(getStatusText(HttpStatus.INTERNAL_SERVER_ERROR));
       }
     }
   }
